ff71a7a2e31bce4c4cc49574504f8ee3bee250b3
ALex Jen <alexjen1999@gmail.com>
2021-09-14 18:20:33 -0400

    Added a lot

---
+++ server.js
@@ -0,0 +1,17 @@
+var express = require('express')
+var app  = express()
+var path = require('path')
+const port = 8081
+
+
+app.use(express.static('public'));
+
+app.get('/', function (req, res){
+    res.sendFile(path.join(__dirname + '/html/home.html'))
+})
+
+
+
+app.listen(port, () => {
+    console.log('App listening att localhost\n')
+})
